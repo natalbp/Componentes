33acf7cada2733d596b77782283aff7ca2a42f1c
Nat <nataliaberpino@gmail.com>
2026-03-04 10:18:36 -0500

    Cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Saludo from './componentes/Saludo'
+import BotonLike from './componentes/BotonLike';
 import TarjetaUsuario from './TarjetaUsuario';
+import PostConEfecto from './componentes/PostConEfecto';
+import MatematicasMagicas from './componentes/MatematicasMagicas';
 
 function App() {
 return (
   <div>
+    <Saludo />
+    <PostConEfecto />
+    <MatematicasMagicas />
+    <BotonLike />
+
     <h1>Lista de Jugadores</h1>
       <TarjetaUsuario nombre="Lucía" juegoFavorito="Valorant" />
       <TarjetaUsuario nombre="Marcos" juegoFavorito="Minecraft" />
